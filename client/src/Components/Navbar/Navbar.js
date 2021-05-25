06785406c6166663290f71bbd8c1a3925698368a
abdelfatah ashour <abdelfatahashour4@gmail.com>
2021-05-25 15:30:27 +0200

    delete variable unused

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Context_API/AuthContext';
 import { ToastContainer } from 'react-toastify';
